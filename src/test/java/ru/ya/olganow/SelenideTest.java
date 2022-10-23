0fefe256faced464e920cf5089746938c5537f2e
olga <olganow@yandex.ru>
2022-10-23 23:44:38 +0400

    Selenide test

---
+++ src/test/java/ru/ya/olganow/SelenideTest.java
@@ -0,0 +1,25 @@
+package ru.ya.olganow;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest {
+
+    @Test
+    public void testIssueSearch(){
+        open("https://github.com");
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("eroshenkoam/allure-example");
+        $(".header-search-input").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $("#issues-tab").click();
+        $(withText("#81")).should(Condition.exist);
+
+    }
+}
