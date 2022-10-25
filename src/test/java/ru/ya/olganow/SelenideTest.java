c86390ef322e99244d0d311674c56726e2fc75d6
olga <olganow@yandex.ru>
2022-10-25 23:28:11 +0400

    updated SelenideTest

--- src/test/java/ru/ya/olganow/SelenideTest.java
+++ src/test/java/ru/ya/olganow/SelenideTest.java
@@ -1,6 +1,8 @@
 package ru.ya.olganow;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.withText;
@@ -12,6 +14,7 @@ public class SelenideTest {
 
     @Test
     public void testIssueSearch(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
         open("https://github.com");
         $(".header-search-input").click();
         $(".header-search-input").sendKeys("eroshenkoam/allure-example");
