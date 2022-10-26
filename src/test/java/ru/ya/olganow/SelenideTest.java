e2a2bbcb90a94265e61d04eedf1f423066900cb6
olga <olganow@yandex.ru>
2022-10-26 23:37:55 +0400

    added SelenideTest for olganow

--- src/test/java/ru/ya/olganow/SelenideTest.java
+++ src/test/java/ru/ya/olganow/SelenideTest.java
@@ -3,18 +3,24 @@ package ru.ya.olganow;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.By.linkText;
 
 public class SelenideTest {
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
 
     @Test
     public void testIssueSearch(){
-        SelenideLogger.addListener("allure", new AllureSelenide());
+
         open("https://github.com");
         $(".header-search-input").click();
         $(".header-search-input").sendKeys("eroshenkoam/allure-example");
@@ -25,4 +31,14 @@ public class SelenideTest {
         $(withText("#81")).should(Condition.exist);
 
     }
-}
+
+    @Test
+    void issueSearch() {
+
+        String repo = "olganow/qa_guru_hw1_gith";
+        open("https://github.com/");
+        $x("//input[contains(@type,'text')]").setValue(repo).submit();
+        $(byText(repo)).click();
+
+        $("#issues-tab").shouldBe(visible);
+    }}
