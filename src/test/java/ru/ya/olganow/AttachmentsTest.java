de61e9bd5e6d94f795174a3da8e2f00a1222c934
olga <olganow@yandex.ru>
2022-10-29 18:55:14 +0400

    step test updated + attacments

---
+++ src/test/java/ru/ya/olganow/AttachmentsTest.java
@@ -0,0 +1,31 @@
+package ru.ya.olganow;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.attachment;
+import static io.qameta.allure.Allure.step;
+
+public class AttachmentsTest {
+
+    @Test
+    public void testLambdaAttachments() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com");
+            attachment("Source", webdriver().driver().source());
+        });
+    }
+
+    @Test
+    public void testAnnotatedAttachments() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        StepTest steps = new StepTest();
+
+        steps.openMainPage();
+        steps.takeScreenshot();
+    }
+}
--- src/test/java/ru/ya/olganow/StepTest.java
+++ src/test/java/ru/ya/olganow/StepTest.java
@@ -1,21 +1,62 @@
 package ru.ya.olganow;
 
+import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
 
 public class StepTest {
+    String repo = "olganow/qa_guru_hw1_gith";
     @BeforeAll
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
-    @Test
-    public void testIssueSearch() {
-
+    @Step("Open the main page")
+        public void openMainPage() {
         open("https://github.com");
     }
+
+    @Step("Find a repo")
+    public void findRepo(String repo) {
+        $x("//input[contains(@type,'text')]").setValue(repo).submit();
+    }
+
+
+    @Step("Click on a repo link")
+    public void clickOnRepo() {
+        $x("//a[contains(@href,'olganow/qa')]").click();
+    }
+
+    @Step("Check issue")
+    public void checkIssue() {
+        $("#issues-tab").shouldBe(visible);
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+
+    @Test
+    public void testAnnotatedStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        StepTest steps = new StepTest();
+
+        steps.openMainPage();
+        steps.findRepo(repo);
+        steps.clickOnRepo();
+        steps.checkIssue();
+
+    }
+
+
 }
